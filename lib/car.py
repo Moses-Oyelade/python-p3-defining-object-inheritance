f86d7313906a2d106bb5a9b486d9af0eb5813c1c
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-11 14:43:15 +0100

    completed

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+volvo = Car(78, "QB789KL")
+# volvo2 = Car(Vehicle)
+
+print(volvo.go())
+# print(volvo.wheel_number)
+# print(volvo.wheel_size)
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,20 @@
 class Vehicle:
-    pass
+    
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+        
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self):
+        return "filling up!"
+    
+toyota = Vehicle(20, "AD2345")
+
+print(toyota.wheel_number)
+print(toyota.wheel_size)
+print(toyota.go())
+print(toyota.fill_up_tank())
+
+
